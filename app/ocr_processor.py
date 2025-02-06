8bbd5dfd323e9d39264fe3596850e9694c4a5b50
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-02-06 17:38:53 +0100

    Update ocr_processor.py

--- app/ocr_processor.py
+++ app/ocr_processor.py
@@ -1,11 +1,21 @@
+from pdf2image import convert_from_path
 import pytesseract
 from PIL import Image
+import os
 
 class Processor:
     def __init__(self, document_path, api_key=None):
         self.document_path = document_path
-    
+
     def extract_data(self):
-        text = pytesseract.image_to_string(Image.open(self.document_path))
-        return {"raw_text": text}
+        # Vérifie si le fichier est un PDF et le convertit en images
+        if self.document_path.lower().endswith(".pdf"):
+            images = convert_from_path(self.document_path)
+            text = ""
+            for i, img in enumerate(images):
+                text += pytesseract.image_to_string(img) + "\n"
+        else:
+            text = pytesseract.image_to_string(Image.open(self.document_path))
+
+        return {"text": text}
 
