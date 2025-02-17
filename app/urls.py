0271a2ff0e7f48df6cfc936e7fa92358d875477b
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-02-17 22:16:17 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import document_view
+from .views import document_view  # Assurez-vous que cette ligne est correcte
 
 urlpatterns = [
-    path('document_views/', document_view, name='document_view'),
+    path('document/', document_view, name='document_view'),
 ]
