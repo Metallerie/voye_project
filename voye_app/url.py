47303568b12f206ddf316b6f02520bcc2df07698
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-03-15 21:48:44 +0100

    Update url.py

--- voye_app/url.py
+++ voye_app/url.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('document/<int:pk>/', views.document_view, name='document_view'),
     path('document/<int:pk>/previous/', views.previous_document, name='previous_document'),
     path('document/<int:pk>/next/', views.next_document, name='next_document'),
+
 ]
