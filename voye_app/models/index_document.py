2d2ea69c539dbd76403094489a2938c59c6ef8c5
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-03-15 21:30:47 +0100

    Update index_document.py

--- voye_app/models/index_document.py
+++ voye_app/models/index_document.py
@@ -1,9 +1,10 @@
 from djongo import models
+from django.urls import reverse
 
 class IndexDocument(models.Model):
     original_filename = models.CharField(max_length=255)
     document_type = models.CharField(max_length=100)
-    json_filename = models.CharField(max_length=255)
+    json_filename = models.JSONField()
     storage_path = models.CharField(max_length=255)
     archive_path = models.CharField(max_length=255)
     partner_name = models.CharField(max_length=255)
@@ -14,3 +15,9 @@ class IndexDocument(models.Model):
 
     class Meta:
         db_table = 'index_document'
+
+    def get_previous_document(self):
+        return IndexDocument.objects.filter(id__lt=self.id).order_by('-id').first()
+
+    def get_next_document(self):
+        return IndexDocument.objects.filter(id__gt=self.id).order_by('id').first()
