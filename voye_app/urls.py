6392df42145bd39a6186c9253adc77c562e65092
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-03-16 11:09:29 +0100

    Update urls.py

--- voye_app/urls.py
+++ voye_app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('document/<int:pk>/', document_view, name='document_view'),
     path('document/<int:pk>/previous/', previous_document, name='previous_document'),
     path('document/<int:pk>/next/', next_document, name='next_document'),
-    path('documents/', document_list, name='document_list'),  # Ajoutez ceci pour la liste des documents
-    path('hello-world/', hello_world, name='hello_world'),  # Ajoutez ceci pour la nouvelle vue
-
+    path('documents/', document_list, name='document_list'),
+    path('hello-world/', hello_world, name='hello_world'),
 ]
