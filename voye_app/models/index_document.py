0c999c0a6bd693d6532508438dfcc1e201f9b9f5
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-03-16 17:59:10 +0100

    Update index_document.py

--- voye_app/models/index_document.py
+++ voye_app/models/index_document.py
@@ -1,6 +1,7 @@
 from djongo import models
 
 class IndexDocument(models.Model):
+    _id = models.ObjectIdField(default=ObjectId, primary_key=True)
     original_filename = models.CharField(max_length=255)
     document_type = models.CharField(max_length=100)
     json_filename = models.JSONField()
