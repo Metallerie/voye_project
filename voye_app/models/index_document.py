d76f5a6f2ffc99392c21794c853e5b209ce080c2
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-03-17 22:49:54 +0100

    Update index_document.py

--- voye_app/models/index_document.py
+++ voye_app/models/index_document.py
@@ -1,1 +1,6 @@
-from djongo import models
+class IndexDocument(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
+    # autres champs...
+
+    class Meta:
+        db_table = "index_document"
