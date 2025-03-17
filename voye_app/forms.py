44e0dc025d23ddd199366c30df6d388b2a650a68
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-03-17 22:54:48 +0100

    Update forms.py

--- voye_app/forms.py
+++ voye_app/forms.py
@@ -5,7 +5,8 @@ from voye_app.models import IndexDocument
 class IndexDocumentForm(forms.ModelForm):
     class Meta:
         model = IndexDocument
-        fields = ['original_filename', 'document_type', 'json_filename', 'storage_path_json', 'archive_path', 'partner_name', 'document_date', 'file_size', 'checksum', 'timestamp']
-        widgets = {
-            'json_filename': JSONEditorWidget
-        }
+       # fields = ['original_filename', 'document_type', 'json_filename', 'storage_path_json', 'archive_path', 'partner_name', 'document_date', 'file_size', 'checksum', 'timestamp']
+        fields = ['id']
+       # widgets = {
+       #     'json_filename': JSONEditorWidget
+        #}
