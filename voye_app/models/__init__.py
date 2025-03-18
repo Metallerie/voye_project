8d5e9509df62507fd2fd2cfcdd26c8a00d0f2800
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-03-18 17:55:05 +0100

    Update __init__.py

--- voye_app/models/__init__.py
+++ voye_app/models/__init__.py
@@ -1,2 +1,6 @@
 from .index_document import IndexDocument
+from .voye_config import VoyeConfig
+from .voye_frontend_config import VoyeFrontendConfig
+
+
 # Importez d'autres modèles ici si nécessaire
