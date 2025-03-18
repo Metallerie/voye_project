1ad0dfdc62d662738e66face358dc708a33f0056
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-03-18 21:20:48 +0100

    Update urls.py

--- voye_app/urls.py
+++ voye_app/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from voye_app.views import document_view, previous_document, next_document, document_list, hello_world
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('document/<int:pk>/', document_view, name='document_view'),
     path('document/<int:pk>/previous/', previous_document, name='previous_document'),
     path('document/<int:pk>/next/', next_document, name='next_document'),
