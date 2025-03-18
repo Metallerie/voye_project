ffa83ef7642d6e40403306786838ab66227ed35b
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-03-18 16:42:25 +0100

    Update voye_config.py

--- voye_app/models/voye_config.py
+++ voye_app/models/voye_config.py
@@ -1,2 +1,16 @@
 from djongo import models
 from bson import ObjectId
+
+from django.db import models
+
+class Voye_Config(models.Model):
+    id = models.AutoField(primary_key=True)
+    key = models.CharField(max_length=255)
+    value = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    write_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.key
